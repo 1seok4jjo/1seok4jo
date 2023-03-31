67db817c309691c4bcb7d3c2798258c24cffe7bb
LEE <dmstn356@naver.com>
2023-03-31 15:44:10 +0900

    테스트-은수

--- src/main/java/team/compass/Controller/HelloWorldController.java
+++ src/main/java/team/compass/Controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String test() {
-        return "제로베이스 김정렬 2023 03 31 15:22분 테스트";
+        return "제로베이스 이은수 2023 03 31 테스트";
     }
 }
