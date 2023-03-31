f6edeed90d38926174e8881e03cadc515e203ae7
jungryuel <zldrktduf0927@gmail.com>
2023-03-31 15:11:53 +0900

    커밋테스트입니다

--- src/main/java/team/compass/Controller/HelloWorldController.java
+++ src/main/java/team/compass/Controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String test() {
-        return "제로베이스 엄성준 2023 03 31 15:05분 테스트";
+        return "제로베이스 김정렬 2023 03 31 15:11분 테스트";
     }
 }
