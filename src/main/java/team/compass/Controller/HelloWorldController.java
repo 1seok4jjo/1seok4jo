c8f29c2af5740910c17a31b0958e55c14ee28593
yeb0 <s110528@nate.com>
2023-04-05 00:18:25 +0900

    Docs : test4

--- src/main/java/team/compass/Controller/HelloWorldController.java
+++ src/main/java/team/compass/Controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String test() {
-        return "마지막 테스트 by hoon 12";
+        return "마지막 테스트 by hoon 123";
     }
 }
