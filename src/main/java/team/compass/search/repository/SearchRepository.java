f1b8013f2025d888316dcaeb9376c6456953f2dc
Youngjo Gil <dudwh921112@naver.com>
2023-04-20 00:06:11 +0900

    Feat: search response 해시태그 추가

--- src/main/java/team/compass/search/repository/SearchRepository.java
+++ src/main/java/team/compass/search/repository/SearchRepository.java
@@ -1,25 +1,20 @@
 package team.compass.search.repository;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.compass.post.domain.Post;
-import team.compass.search.dto.SearchRequest;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SearchRepository extends JpaRepository<Post, Integer> {
+    // ignoreCase 대소문자 구분 x
     // 제목
-    Optional<Page<Post>> findAllByTitleContainingOrderByCreatedAtDesc(String title, Pageable pageable);
+    Optional<Page<Post>> findAllByTitleContainingIgnoreCaseOrderByCreatedAtDesc(String title, Pageable pageable);
     // 내용
-    Optional<Page<Post>> findAllByDetailContainingOrderByCreatedAtDesc(String detail, Pageable pageable);
+    Optional<Page<Post>> findAllByDetailContainingIgnoreCaseOrderByCreatedAtDesc(String detail, Pageable pageable);
     // 해시태그
-    Optional<Page<Post>> findAllByHashtagContainingOrderByCreatedAtDesc(String hashTag, Pageable pageable);
-
-//    Optional<Page<Post>> findAllByTitleContainingOrDetailContainingOrHashtagContainingOrderByCreatedAtDesc(String keyword, String keyword2, String keyword3,  Pageable pageable);
+    Optional<Page<Post>> findAllByHashtagContainingIgnoreCaseOrderByCreatedAtDesc(String hashTag, Pageable pageable);
 }
