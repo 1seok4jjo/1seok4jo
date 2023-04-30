204474845206ae373d8091d94a4314280c584e63
yeb0 <s110528@nate.com>
2023-04-30 23:18:14 +0900

    Refactor : UserRequest 생성자, 좋아요 기능

--- src/main/java/team/compass/like/repository/LikeRepository.java
+++ src/main/java/team/compass/like/repository/LikeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 import team.compass.like.domain.Likes;
+import team.compass.post.domain.Post;
+import team.compass.user.domain.User;
 
 
 import java.util.Optional;
@@ -13,4 +15,6 @@ public interface LikeRepository extends JpaRepository<Likes, Integer> {
 
     Optional<Likes> findAllByPost_IdAndUser_Id(Integer post_id,
                                                Integer user_id);
+
+    Optional<Likes> findByUserAndPost(User user, Post post);
 }
